85978a2ec0a1c5aca1e1486e1452cc67ce169e04
Jan Vincent Liwanag <jvliwanag@gmail.com>
2022-09-13 17:20:32 +0800

    Remove Box for rust target
    * This breaks support for recursive types

--- crates/target_rust/src/lib.rs
+++ crates/target_rust/src/lib.rs
@@ -128,7 +128,11 @@ impl jtd_codegen::target::Target for Target {
             // reference detector, and insert Box<T> only if it's necessary to
             // break a cyclic dependency. It's unclear how much of a problem
             // this is in the real world.
-            target::Expr::NullableOf(sub_expr) => format!("Option<Box<{}>>", sub_expr),
+            //
+            // target::Expr::NullableOf(sub_expr) => format!("Option<Box<{}>>", sub_expr),
+            //
+            // Remove Box for now. Readd this should recursive types be needed
+            target::Expr::NullableOf(sub_expr) => format!("Option<{}>", sub_expr),
         }
     }
 
